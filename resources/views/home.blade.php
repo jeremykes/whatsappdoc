711c8ff5c99025ac94cd3158aa5e1f19a1f88aad
Jeremy Kes Palme <jeremypalme@gmail.com>
2022-12-04 22:26:00 +1000

    First draft

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div class="container mx-auto px-4">
+        <div class="columns-2">
+            <div class="bg-"></div>
+            <div></div>
+        </div>
+    </div>
+</body>
+</html>
